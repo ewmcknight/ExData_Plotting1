8b99ae38af50911c4cb544ce38e7f2570d1494e5
ewmcknight <ericmcknight@Erics-MacBook-Pro.local>
2015-09-12 20:15:32 -0700

    Initial push

---
+++ plot1.R
@@ -0,0 +1,31 @@
+# E.W. McKnight
+# 9/12/2015
+#
+# For course "Exploratory Data Analysis", Roger D. Peng, PhD, et al
+#       Johns Hopkins Bloomberg School of Public Health
+#       Coursera "Data Science Specialization"
+#
+# Data: “Individual household electric power consumption Data Set”
+#       https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+#       Courtesy of UC Irvine Machine Learning Repository, http://archive.ics.uci.edu/ml/
+#
+# Plots a histogram of Global_active_power between 2007-02-01 and 2007-02-02
+#
+
+library(dplyr)
+
+# Important data is located within the first 70,000 rows.
+data = read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
+                  na.strings="?", dec = ".", fill = TRUE, nrows = 70000)
+
+data <- data %>%
+    mutate(Date_Time = as.POSIXct(paste(as.Date(Date, "%d/%m/%Y"),Time))) %>%
+    filter(Date_Time >= as.POSIXct("2007-02-01 00:00"), 
+           Date_Time <= as.POSIXct("2007-02-02 23:59")) %>%
+    select(Date_Time, Global_active_power: Sub_metering_3)
+
+png(file = "plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, breaks = 12, col="red",
+     xlab="Global Active Power (kilowatts)",
+     main="Global Active Power")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,62 @@
+# E.W. McKnight
+# 9/12/2015
+#
+# For course "Exploratory Data Analysis", Roger D. Peng, PhD, et al
+#       Johns Hopkins Bloomberg School of Public Health
+#       Coursera "Data Science Specialization"
+#
+# Data: “Individual household electric power consumption Data Set”
+#       https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+#       Courtesy of UC Irvine Machine Learning Repository, http://archive.ics.uci.edu/ml/
+#
+# Plots Global_active_power vs time between 2007-02-01 and 2007-02-02
+#       Sub_metering_1, _2, _3 vs time between 2007-02-01 and 2007-02-02
+#       Voltage vs time between 2007-02-01 and 2007-02-02
+#       Global_reactive_power vs time between 2007-02-01 and 2007-02-02
+
+library(dplyr)
+
+# Important data is located within the first 70,000 rows.
+data = read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
+                  na.strings="?", dec = ".", fill = TRUE, nrows = 70000)
+
+data <- data %>%
+    mutate(Date_Time = as.POSIXct(paste(as.Date(Date, "%d/%m/%Y"),Time))) %>%
+    filter(Date_Time >= as.POSIXct("2007-02-01 00:00"), 
+           Date_Time <= as.POSIXct("2007-02-02 23:59")) %>%
+    select(Date_Time, Global_active_power: Sub_metering_3)
+
+png(file = "plot4.png", width = 480, height = 480)
+par(mfcol = c(2,2))
+
+# from plot2.R (truncating y-axis label)
+plot(data$Global_active_power~data$Date_Time, type = "l",
+     xlab = "",
+     ylab="Global Active Power",
+     main="")
+
+# from plot3.R (removing legend border)
+plot(data$Sub_metering_1~data$Date_Time, type = "l",
+     xlab = "",
+     ylab="Energy sub metering",
+     main="")
+lines(data$Sub_metering_2~data$Date_Time, col = "red")
+lines(data$Sub_metering_3~data$Date_Time, col = "blue")
+legend("topright", bty = "n",
+       col = c("black", "red", "blue"),
+       lty = c(1, 1, 1),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# Voltage plot
+plot(data$Voltage~data$Date_Time, type = "l",
+     xlab = "datetime",
+     ylab="Voltage",
+     main="")
+
+# Global_reactive_power plot
+plot(data$Global_reactive_power~data$Date_Time, type = "l",
+     xlab = "datetime",
+     ylab="Global_reactive_power",
+     main="")
+
+dev.off()
